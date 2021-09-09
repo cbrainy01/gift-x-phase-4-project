a417c71390a1a018b442b8dc71feb832ee3b3f91
Carl <carlbrainegebe@gmail.com>
2021-09-09 13:16:19 -0500

    migrate user table and create seed data for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create(username: "mike", password: "apple", password_confirmation: "apple", name: "Michael Scott")
+user2 = User.create(username: "sam", password: "apple", password_confirmation: "apple", name: "Samuel Jackson")
+user3 = User.create(username: "tim", password: "apple", password_confirmation: "apple", name: "Timothey Smith")
