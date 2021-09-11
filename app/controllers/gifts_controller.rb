1518da6c34987aba73805767bd81f13b9449aeb9
Carl <carlbrainegebe@gmail.com>
2021-09-11 02:00:55 -0500

    create gifts controller and serializer

---
+++ app/controllers/gifts_controller.rb
@@ -0,0 +1,17 @@
+class GiftsController < ApplicationController
+
+    def show
+        # gift = Gift.find(params[:id])
+    end
+    
+    def index
+
+    end
+
+    private
+
+    def gift_params
+
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get '/me', to: 'users#show'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  resources :gifts
   
   # post "/signup", to: "users#create"
 end
